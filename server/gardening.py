63d03c882c55ec7bad70cd1fb3f8c9ca224c6e11
Takashi Ando <takashi5.ando@toshiba.co.jp>
2014-09-29 13:10:59 +0900

    Add GardeningMonitor process class and server.py to use it for issue #4.

--- server/gardening.py
+++ server/gardening.py
@@ -5,101 +5,113 @@
  Editor     : Takashi Ando
  Version    : 1.0
 """
-from xbee import ZigBee
-from requests.exceptions import HTTPError
 import sys
 import datetime
 import struct
 import time
+import multiprocessing
 import serial
 import xively
+from xbee import ZigBee
+from requests.exceptions import HTTPError
 
-_XBEE_PORT = '/dev/ttyAMA0'
-_XBEE_BAUDRATE = 9600
-
-_XIVELY_FEED_ID = 1779591762
-_XIVELY_ID_TABLE = \
-{
-        'LUX0':'Luminosity',
-        'HUM0':'Humidity',
-        'MOI0':'MoistureOfSmallPlant',
-        'MOI1':'MoistureOfLargePlant',
-        'TMP0':'Temperature',
-}
-
-def message_received(feed, data, now):
-    print(now)
-
-    source_addr_long_raw = [byte for byte in struct.unpack('BBBBBBBB', data['source_addr_long'])]
-
-    source_addr_long_high = 0
-    for i in range(0,4):
-        source_addr_long_high |= source_addr_long_raw[i] << 8 * (3 - i)
-    print(hex(source_addr_long_high))
-
-    source_addr_long_low = 0
-    for i in range(4,8):
-        source_addr_long_low |= source_addr_long_raw[i] << 8 * (7 - i)
-    print(hex(source_addr_long_low))
+class GardeningMonitor(multiprocessing.Process):
+    _XBEE_PORT = '/dev/ttyAMA0'
+    _XBEE_BAUDRATE = 9600
     
-    source_addr_raw = [byte for byte in struct.unpack('BB', data['source_addr'])]
+    _XIVELY_FEED_ID = 1779591762
+    _XIVELY_ID_TABLE = \
+    {
+            'LUX0':'Luminosity',
+            'HUM0':'Humidity',
+            'MOI0':'MoistureOfSmallPlant',
+            'MOI1':'MoistureOfLargePlant',
+            'TMP0':'Temperature',
+    }
 
-    source_addr = 0
-    for i in range(0,2):
-        source_addr |= source_addr_raw[i] << 8 * (1 - i)
-    print(hex(source_addr))
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
+        multiprocessing.Process.__init__(self, group=group, target=target,\
+                name=name, args=args, kwargs=kwargs, daemon=None)
 
-    #print(data['id'])
-    #print(data['options'])
+        ser = serial.Serial(self._XBEE_PORT, self._XBEE_BAUDRATE)
 
-    rf_data = struct.unpack('4sl4sl4sl4sl4sl', data['rf_data'])
-
-    datastreams = []
-    for rf_word_num in range(0,5):
-        sensor_type  = rf_data[rf_word_num*2+0].decode('ascii')
-        sensor_value = rf_data[rf_word_num*2+1] / 10.0
-        print(sensor_type, sensor_value)
-
-        datastreams.append(xively.Datastream(id=_XIVELY_ID_TABLE[sensor_type], current_value=sensor_value, at=now))
-
-    feed.datastreams = datastreams
-    feed.update()
-
-if __name__ == '__main__':
-    ser = serial.Serial(_XBEE_PORT, _XBEE_BAUDRATE)
-    
-    kargs = {}
-    kargs['escaped'] = True
-    #kargs['callback'] = message_received
-    
-    if len(sys.argv) > 1:
-        if 'debug' in sys.argv:
-            xbee = None
-        else:
-            xbee = ZigBee(ser, **kargs)
-            api = xively.XivelyAPIClient(sys.argv[1])
-            feed = api.feeds.get(_XIVELY_FEED_ID)
-    else:
-        raise SystemError('Xively API key is required.')
-    
-    # Do other stuff in the main thread
-    while True:
-        try:
-            if xbee is None:
-                ser.read()
+        kargs = {}
+        kargs['escaped'] = True
+        #kargs['callback'] = message_received
+        
+        if len(sys.argv) > 1:
+            if 'debug' in sys.argv:
+                xbee = None
             else:
-                data = xbee.wait_read_frame()
-                now = datetime.datetime.utcnow()
-                message_received(feed, data, now)
-        except HTTPError:
-            print('HTTPError occurs.')
-            time.sleep(600)
-            continue
-        except KeyboardInterrupt:
-            break
+                xbee = xbee.ZigBee(ser, **kargs)
+                api = xively.XivelyAPIClient(sys.argv[1])
+                feed = api.feeds.get(_XIVELY_FEED_ID)
+        else:
+            raise SystemError('Xively API key is required.')
+
+    def run(self):
+        '''
+        Method to be run in sub-process; can be overridden in sub-class
+        '''
+        if self._target:
+            self._target(*self._args, **self._kwargs)
+        else:
+            while True:
+                try:
+                    if xbee is None:
+                        ser.read()
+                    else:
+                        data = xbee.wait_read_frame()
+                        now = datetime.datetime.utcnow()
+                        message_received(feed, data, now)
+                except HTTPError:
+                    print('HTTPError occurs.')
+                    time.sleep(600)
+                    continue
+                except KeyboardInterrupt:
+                    break
+
+            # halt() must be called before closing the serial
+            # port in order to ensure proper thread shutdown
+            if xbee is not None:
+            xbee.halt()
+            ser.close()
+
+    def message_received(feed, data, now):
+        print(now)
     
-    # halt() must be called before closing the serial
-    # port in order to ensure proper thread shutdown
-    if xbee is not None:
-        xbee.halt()
-    ser.close()
+        source_addr_long_raw = [byte for byte in struct.unpack('BBBBBBBB', data['source_addr_long'])]
+    
+        source_addr_long_high = 0
+        for i in range(0,4):
+            source_addr_long_high |= source_addr_long_raw[i] << 8 * (3 - i)
+        print(hex(source_addr_long_high))
+    
+        source_addr_long_low = 0
+        for i in range(4,8):
+            source_addr_long_low |= source_addr_long_raw[i] << 8 * (7 - i)
+        print(hex(source_addr_long_low))
+        
+        source_addr_raw = [byte for byte in struct.unpack('BB', data['source_addr'])]
+    
+        source_addr = 0
+        for i in range(0,2):
+            source_addr |= source_addr_raw[i] << 8 * (1 - i)
+        print(hex(source_addr))
+    
+        #print(data['id'])
+        #print(data['options'])
+    
+        rf_data = struct.unpack('4sl4sl4sl4sl4sl', data['rf_data'])
+    
+        datastreams = []
+        for rf_word_num in range(0,5):
+            sensor_type  = rf_data[rf_word_num*2+0].decode('ascii')
+            sensor_value = rf_data[rf_word_num*2+1] / 10.0
+            print(sensor_type, sensor_value)
+    
+            datastreams.append(xively.Datastream(id=_XIVELY_ID_TABLE[sensor_type], current_value=sensor_value, at=now))
+    
+        feed.datastreams = datastreams
+        feed.update()
+
---
+++ server/server.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import gardening
+
+if __name__ == '__main__':
+    # Do other stuff in the main thread
+    pass
