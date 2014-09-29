903ee9612e5d39ad05e9a329f93d76479c21c737
Takashi Ando <dodo5522@gmail.com>
2014-09-30 04:55:07 +0900

    Remove spaces.

--- server/gardening.py
+++ server/gardening.py
@@ -18,15 +18,15 @@ from requests.exceptions import HTTPError
 class GardeningMonitor(multiprocessing.Process):
     _XBEE_PORT = '/dev/ttyAMA0'
     _XBEE_BAUDRATE = 9600
-    
+
     _XIVELY_FEED_ID = 1779591762
     _XIVELY_ID_TABLE = \
     {
-            'LUX0':'Luminosity',
-            'HUM0':'Humidity',
-            'MOI0':'MoistureOfSmallPlant',
-            'MOI1':'MoistureOfLargePlant',
-            'TMP0':'Temperature',
+        'LUX0':'Luminosity',
+        'HUM0':'Humidity',
+        'MOI0':'MoistureOfSmallPlant',
+        'MOI1':'MoistureOfLargePlant',
+        'TMP0':'Temperature',
     }
 
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
@@ -75,39 +75,39 @@ class GardeningMonitor(multiprocessing.Process):
 
     def _parse_xbee_data(data, now):
         print(now)
-    
+
         source_addr_long_raw = [byte for byte in struct.unpack('BBBBBBBB', data['source_addr_long'])]
-    
+
         source_addr_long_high = 0
         for i in range(0,4):
             source_addr_long_high |= source_addr_long_raw[i] << 8 * (3 - i)
         print(hex(source_addr_long_high))
-    
+
         source_addr_long_low = 0
         for i in range(4,8):
             source_addr_long_low |= source_addr_long_raw[i] << 8 * (7 - i)
         print(hex(source_addr_long_low))
         
         source_addr_raw = [byte for byte in struct.unpack('BB', data['source_addr'])]
-    
+
         source_addr = 0
         for i in range(0,2):
             source_addr |= source_addr_raw[i] << 8 * (1 - i)
         print(hex(source_addr))
-    
+
         #print(data['id'])
         #print(data['options'])
-    
+
         rf_data = struct.unpack('4sl4sl4sl4sl4sl', data['rf_data'])
-    
+
         datastreams = []
         for rf_word_num in range(0,5):
             sensor_type  = rf_data[rf_word_num*2+0].decode('ascii')
             sensor_value = rf_data[rf_word_num*2+1] / 10.0
             print(sensor_type, sensor_value)
-    
+
             datastreams.append(xively.Datastream(id=_XIVELY_ID_TABLE[sensor_type], current_value=sensor_value, at=now))
-    
+
         feed.datastreams = datastreams
         feed.update()
 
