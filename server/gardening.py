0086f02ebcbc922b700e211ddc28a63008f08150
Takashi Ando <dodo5522@gmail.com>
2014-10-10 01:25:22 +0900

    To use base monitor.

--- server/gardening.py
+++ server/gardening.py
@@ -6,70 +6,45 @@
  Version    : 1.0
 """
 
-import sys
-import datetime
-import struct
 import time
 import multiprocessing
-import xively
-from requests.exceptions import HTTPError
-from xbeereceiver import XBeeApiFrameParser
+import logging
+import xbeereceiver
+import basemonitor
 
-class GardeningMonitor(multiprocessing.Process, XBeeApiFrameParser):
+class VegetablesPlanterMonitor(
+        multiprocessing.Process,
+        xbeereceiver.XBeeApiRfDataParser,
+        basemonitor.BaseMonitor):
     '''
     This class monitors gardening status with XBee.
     And put the information to Xively.
     '''
+    def __init__(self, source_addr_long=None,\
+            xively_api_key=None, xively_feed_id=None,\
+            log_level=logging.INFO):
+        '''
+        Initialize process etc.
+        '''
+        multiprocessing.Process.__init__(self, name=type(self).__name__)
 
-    _XIVELY_FEED_ID = 1779591762
-    _XIVELY_ID_TABLE = \
-    {
-        'LUX0':'Luminosity',
-        'HUM0':'Humidity',
-        'MOI0':'MoistureOfSmallPlant',
-        'MOI1':'MoistureOfLargePlant',
-        'TMP0':'Temperature',
-    }
-
-    def __init__(self, target=None, name=None, args=(), kwargs={}):
-        multiprocessing.Process.__init__(self, \
-                target=target, name=name, args=args, kwargs=kwargs, daemon=None)
-
-        if len(sys.argv) > 1:
-            api = xively.XivelyAPIClient(sys.argv[1])
-            feed = api.feeds.get(_XIVELY_FEED_ID)
-        else:
-            raise SystemError('Xively API key is required.')
+        basemonitor.BaseMonitor(self,
+                source_addr_long=source_addr_long,
+                xively_api_key=xively_api_key,
+                xively_feed_id=xively_feed_id,
+                log_level=log_level)
 
     def run(self):
         '''
         Method to be run in sub-process; can be overridden in sub-class
         '''
-        if self._target:
-            self._target(*self._args, **self._kwargs)
-        else:
+        while True:
             try:
-                if xbee is None:
-                    ser.read()
-                else:
-                    datastreams = []
-                    parse()
-                    now = datetime.datetime.utcnow()
-                    self._logger.info(now)
-                    datastreams.append(xively.Datastream(id=_XIVELY_ID_TABLE[sensor_type], current_value=sensor_value, at=now))
-                    feed.datastreams = datastreams
-                    feed.update()
-            except HTTPError:
-                print('HTTPError occurs.')
-                time.sleep(600)
-                continue
+                pass
             except KeyboardInterrupt:
                 break
-
-            # halt() must be called before closing the serial
-            # port in order to ensure proper thread shutdown
-            if xbee is not None:
-                xbee.halt()
-            ser.close()
-
+            except Exception as err:
+                print('{0} occurs.'.format(err))
+                time.sleep(600)
+                continue
 
