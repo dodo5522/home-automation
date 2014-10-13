313ae4f66c104b0d48b63574688638dbf716ba6e
Takashi Ando <dodo5522@gmail.com>
2014-10-14 06:31:17 +0900

    Add test code to process.py.

--- server/process.py
+++ server/process.py
@@ -9,12 +9,10 @@ import multiprocessing
 import logging
 
 class BaseProcess(multiprocessing.Process):
-    '''
-    Base class to generate process.
+    '''Base class to generate process.
     '''
     def __init__(self, log_level=None):
-        '''
-        Initialize process.
+        '''Initialize process.
         '''
         self._logger = logging.getLogger(type(self).__name__)
         self._logger.setLevel(log_level)
@@ -27,13 +25,11 @@ class BaseProcess(multiprocessing.Process):
         self._message_table[0] = self._do_terminate
 
     def run(self):
-        '''
-        Message handler.
+        '''Parse message queue.
         '''
         while True:
             item = self._message_queue.get()
             self._logger.debug('got message id {0}.'.format(item[0]))
-            print('got message id {0}.'.format(item[0]))
 
             func = self._message_table[item[0]]
             if item[1] is None:
@@ -41,26 +37,57 @@ class BaseProcess(multiprocessing.Process):
             else:
                 func(item[1])
 
+            # id 0 means termination.
             if item[0] == 0:
                 break
 
     def terminate(self):
+        '''Post message to terminate this process.
+           You shoul wait for joined with join() method.
         '''
-        Post message to terminate this process.
-        You should wait for joined with join() method.
-        '''
-        self._message_queue.put((0, None))
+        self._message_queue.put_nowait((0, None))
 
     def _do_terminate(self):
+        '''Terminate procedure should be implemented.
         '''
-        Terminate internally.
-        '''
-        #raise NotImplementedError
-        print('terminate internally.')
+        raise NotImplementedError
 
 if __name__ == '__main__':
-    p = BaseProcess(log_level=logging.DEBUG)
-    p.start()
-    p.terminate()
-    p.join()
+    import time
+
+    class TestChildProcess1(BaseProcess):
+        def terminate(self):
+            self._message_queue.put_nowait((0, 10))
+            print('{0}: sleep 10 terminate message is posted.'.format(self.name))
+
+        def _do_terminate(self, wait):
+            for i in range(0, wait):
+                print('{0}: terminating.'.format(self.name))
+                time.sleep(1)
+            print('{0}: terminated.'.format(self.name))
+
+    class TestChildProcess2(BaseProcess):
+        def terminate(self):
+            self._message_queue.put_nowait((0, 5))
+            print('{0}: sleep 5 terminate message is posted.'.format(self.name))
+
+        def _do_terminate(self, wait):
+            for i in range(0, wait):
+                print('{0}: terminating.'.format(self.name))
+                time.sleep(1)
+            print('{0}: terminated.'.format(self.name))
+
+    p = []
+    p.append(TestChildProcess1(log_level=logging.DEBUG))
+    p.append(TestChildProcess2(log_level=logging.DEBUG))
+
+    for process in p:
+        process.start()
+
+    # post terminate message to all process
+    for process in p:
+        process.terminate()
+
+    for process in p:
+        process.join()
 
