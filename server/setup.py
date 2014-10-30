ce5e279537be99ec5e3bf063e3e9f0363d47c7b0
Takashi Ando <dodo5522@gmail.com>
2014-10-30 23:33:58 +0900

    Add description etc to setup.py

--- server/setup.py
+++ server/setup.py
@@ -3,6 +3,10 @@ from distutils.core import setup
 setup(
     name='home_automation',
     version='1.0',
+    description='Python modules to manage the home automation',
+    author='Takashi Ando',
+    author_email='dodo5522@gmail.com',
+    url='http://qiita.com/dodo5522/items/63d1efee3f70b3d5f2f6',
     package_dir={
         'home_automation':'home-automation',
         'home_automation.base':'home-automation/base'
