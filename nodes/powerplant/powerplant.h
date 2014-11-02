ad1b979a4395c0d2222d37285b7c132c936eb20f
Takashi Ando <dodo5522@gmail.com>
2014-11-02 14:24:05 +0900

    Implement to read current/voltage sensor values.

--- nodes/powerplant/powerplant.h
+++ nodes/powerplant/powerplant.h
@@ -31,5 +31,82 @@ typedef enum _SENSOR_ARRAY_NUM
     E_SENSOR_MAX,
 }SENSOR_ARRAY_NUM;
 
+class IElectricPower
+{
+public:
+    virtual void begin(void);
+
+    virtual boolean hasAvilityCurrent(void);
+    virtual boolean hasAvilityVoltage(void);
+
+    virtual float getBusVoltage_V(void);
+    virtual float getShuntVoltage_V(void);
+    virtual float getShuntCurrent_A(void);
+
+    virtual void showConfig(void);
+    virtual void showStatus(void);
+};
+
+class MyINA226 : public IElectricPower
+{
+public:
+    MyINA226(char *name,
+            uint8_t slave_address,
+            float rShuntValueOhm = 0.002,
+            float iMaxExceptedCurrent_A = 2.0,
+            ina226_averages_t avg = INA226_AVERAGES_1,
+            ina226_busConvTime_t busConvTime = INA226_BUS_CONV_TIME_1100US,
+            ina226_shuntConvTime_t shuntConvTime = INA226_SHUNT_CONV_TIME_1100US,
+            ina226_mode_t mode = INA226_MODE_SHUNT_BUS_CONT);
+    virtual ~MyINA226(void){};
+
+    virtual void begin(void) final;
+
+    virtual void showConfig(void) final;
+    virtual void showStatus(void) final;
+
+    virtual boolean hasAvilityCurrent(void) final { return true; };
+    virtual boolean hasAvilityVoltage(void) final { return true; };
+
+    virtual float getBusVoltage_V(void) final;
+    virtual float getShuntVoltage_V(void) final;
+    virtual float getShuntCurrent_A(void) final;
+    float getBusPower_W(void);
+
+private:
+    INA226 ina_;
+    char name_[16];
+    uint8_t slave_address_;
+    ina226_averages_t averages_;
+    ina226_busConvTime_t busConvTime_;
+    ina226_shuntConvTime_t shuntConvTime_;
+    ina226_mode_t mode_;
+    float rShuntValueOhm_;
+    float iMaxExceptedCurrent_A_;
+};
+
+class MyINA219 : public IElectricPower
+{
+public:
+    MyINA219(char *name, uint8_t slave_address);
+    virtual ~MyINA219(void){};
+
+    virtual void begin(void) final;
+
+    virtual void showConfig(void) final {};
+    virtual void showStatus(void) final;
+
+    virtual boolean hasAvilityCurrent(void) final { return true; };
+    virtual boolean hasAvilityVoltage(void) final { return false; };
+
+    virtual float getBusVoltage_V(void) final;
+    virtual float getShuntVoltage_V(void);
+    virtual float getShuntCurrent_A(void) final;
+
+private:
+    Adafruit_INA219 ina_;
+    char name_[16];
+};
+
 #endif
 
--- nodes/powerplant/powerplant.ino
+++ nodes/powerplant/powerplant.ino
@@ -28,12 +28,182 @@
 #define INA219_A1_ADDRESS           (0x44)
 #define INA219_A0A1_ADDRESS         (0x45)
 
+/****************************
+ * MyINA226 class implementation
+ ****************************/
+MyINA226::MyINA226(char *name,
+        uint8_t slave_address,
+        float rShuntValueOhm,
+        float iMaxExceptedCurrent_A,
+        ina226_averages_t averages,
+        ina226_busConvTime_t busConvTime,
+        ina226_shuntConvTime_t shuntConvTime,
+        ina226_mode_t mode)
+    :ina_(),
+    slave_address_(slave_address),
+    averages_(averages),
+    busConvTime_(busConvTime),
+    mode_(mode),
+    rShuntValueOhm_(rShuntValueOhm),
+    iMaxExceptedCurrent_A_(iMaxExceptedCurrent_A)
+{
+    strncpy(name_, name, sizeof(name_) - 1);
+}
+
+void MyINA226::begin(void)
+{
+    ina_.begin(slave_address_);
+    ina_.configure(averages_, busConvTime_, shuntConvTime_, mode_);
+    ina_.calibrate(rShuntValueOhm_, iMaxExceptedCurrent_A_);
+}
+
+void MyINA226::showConfig(void)
+{
+    DEBUG_PRINT("Name:                  "); DEBUG_PRINT(name_); DEBUG_PRINT("\n");
+    DEBUG_PRINT("Mode:                  ");
+    switch (ina_.getMode())
+    {
+        case INA226_MODE_POWER_DOWN:      DEBUG_PRINT("Power-Down\n"); break;
+        case INA226_MODE_SHUNT_TRIG:      DEBUG_PRINT("Shunt Voltage, Triggered\n"); break;
+        case INA226_MODE_BUS_TRIG:        DEBUG_PRINT("Bus Voltage, Triggered\n"); break;
+        case INA226_MODE_SHUNT_BUS_TRIG:  DEBUG_PRINT("Shunt and Bus, Triggered\n"); break;
+        case INA226_MODE_ADC_OFF:         DEBUG_PRINT("ADC Off\n"); break;
+        case INA226_MODE_SHUNT_CONT:      DEBUG_PRINT("Shunt Voltage, Continuous\n"); break;
+        case INA226_MODE_BUS_CONT:        DEBUG_PRINT("Bus Voltage, Continuous\n"); break;
+        case INA226_MODE_SHUNT_BUS_CONT:  DEBUG_PRINT("Shunt and Bus, Continuous\n"); break;
+        default: DEBUG_PRINT("unknown\n");
+    }
+
+    DEBUG_PRINT("Samples average:       ");
+    switch (ina_.getAverages())
+    {
+        case INA226_AVERAGES_1:           DEBUG_PRINT("1 sample\n"); break;
+        case INA226_AVERAGES_4:           DEBUG_PRINT("4 samples\n"); break;
+        case INA226_AVERAGES_16:          DEBUG_PRINT("16 samples\n"); break;
+        case INA226_AVERAGES_64:          DEBUG_PRINT("64 samples\n"); break;
+        case INA226_AVERAGES_128:         DEBUG_PRINT("128 samples\n"); break;
+        case INA226_AVERAGES_256:         DEBUG_PRINT("256 samples\n"); break;
+        case INA226_AVERAGES_512:         DEBUG_PRINT("512 samples\n"); break;
+        case INA226_AVERAGES_1024:        DEBUG_PRINT("1024 samples\n"); break;
+        default: DEBUG_PRINT("unknown\n");
+    }
+
+    DEBUG_PRINT("Bus conversion time:   ");
+    switch (ina_.getBusConversionTime())
+    {
+        case INA226_BUS_CONV_TIME_140US:  DEBUG_PRINT("140uS\n"); break;
+        case INA226_BUS_CONV_TIME_204US:  DEBUG_PRINT("204uS\n"); break;
+        case INA226_BUS_CONV_TIME_332US:  DEBUG_PRINT("332uS\n"); break;
+        case INA226_BUS_CONV_TIME_588US:  DEBUG_PRINT("558uS\n"); break;
+        case INA226_BUS_CONV_TIME_1100US: DEBUG_PRINT("1.100ms\n"); break;
+        case INA226_BUS_CONV_TIME_2116US: DEBUG_PRINT("2.116ms\n"); break;
+        case INA226_BUS_CONV_TIME_4156US: DEBUG_PRINT("4.156ms\n"); break;
+        case INA226_BUS_CONV_TIME_8244US: DEBUG_PRINT("8.244ms\n"); break;
+        default: DEBUG_PRINT("unknown\n");
+    }
+
+    DEBUG_PRINT("Shunt conversion time: ");
+    switch (ina_.getShuntConversionTime())
+    {
+        case INA226_SHUNT_CONV_TIME_140US:  DEBUG_PRINT("140uS\n"); break;
+        case INA226_SHUNT_CONV_TIME_204US:  DEBUG_PRINT("204uS\n"); break;
+        case INA226_SHUNT_CONV_TIME_332US:  DEBUG_PRINT("332uS\n"); break;
+        case INA226_SHUNT_CONV_TIME_588US:  DEBUG_PRINT("558uS\n"); break;
+        case INA226_SHUNT_CONV_TIME_1100US: DEBUG_PRINT("1.100ms\n"); break;
+        case INA226_SHUNT_CONV_TIME_2116US: DEBUG_PRINT("2.116ms\n"); break;
+        case INA226_SHUNT_CONV_TIME_4156US: DEBUG_PRINT("4.156ms\n"); break;
+        case INA226_SHUNT_CONV_TIME_8244US: DEBUG_PRINT("8.244ms\n"); break;
+        default: DEBUG_PRINT("unknown\n");
+    }
+
+    DEBUG_PRINT("Max possible current:  "); DEBUG_PRINT(ina_.getMaxPossibleCurrent()); DEBUG_PRINT(" A\n");
+    DEBUG_PRINT("Max current:           "); DEBUG_PRINT(ina_.getMaxCurrent());         DEBUG_PRINT(" A\n");
+    DEBUG_PRINT("Max shunt voltage:     "); DEBUG_PRINT(ina_.getMaxShuntVoltage());    DEBUG_PRINT(" V\n");
+    DEBUG_PRINT("Max power:             "); DEBUG_PRINT(ina_.getMaxPower());           DEBUG_PRINT(" W\n");
+}
+
+void MyINA226::showStatus(void)
+{
+    DEBUG_PRINT(name_);DEBUG_PRINT("\n");
+    DEBUG_PRINT(" bus V: "); DEBUG_PRINT(getBusVoltage_V());   DEBUG_PRINT("\n");
+    DEBUG_PRINT(" shu V: "); DEBUG_PRINT(getShuntVoltage_V()); DEBUG_PRINT("\n");
+    DEBUG_PRINT(" shu A: "); DEBUG_PRINT(getShuntCurrent_A()); DEBUG_PRINT("\n");
+    DEBUG_PRINT(" pwr W: "); DEBUG_PRINT(getBusPower_W());     DEBUG_PRINT("\n");
+}
+
+inline float MyINA226::getBusVoltage_V(void)
+{
+    return ina_.readBusVoltage();
+}
+
+inline float MyINA226::getShuntVoltage_V(void)
+{
+    return ina_.readShuntVoltage();
+}
+
+inline float MyINA226::getShuntCurrent_A(void)
+{
+    return ina_.readShuntCurrent();
+}
+
+inline float MyINA226::getBusPower_W(void)
+{
+    return ina_.readBusPower();
+}
+
+/****************************
+ * MyINA219 class implementation
+ ****************************/
+MyINA219::MyINA219(char *name, uint8_t slave_address)
+    :ina_(slave_address)
+{
+    strncpy(name_, name, sizeof(name_) - 1);
+}
+
+void MyINA219::begin(void)
+{
+    ina_.begin();
+}
+
+void MyINA219::showStatus(void)
+{
+    DEBUG_PRINT(name_);DEBUG_PRINT("\n");
+    DEBUG_PRINT(" bus V: "); DEBUG_PRINT(getBusVoltage_V());   DEBUG_PRINT("\n");
+    DEBUG_PRINT(" shu V: "); DEBUG_PRINT(getShuntVoltage_V()); DEBUG_PRINT("\n");
+    DEBUG_PRINT(" cur A: "); DEBUG_PRINT(getShuntCurrent_A()); DEBUG_PRINT("\n");
+}
+
+float MyINA219::getBusVoltage_V(void)
+{
+    return ina_.getBusVoltage_V();
+}
+
+float MyINA219::getShuntVoltage_V(void)
+{
+    return ina_.getShuntVoltage_mV()/1000.0;
+}
+
+float MyINA219::getShuntCurrent_A(void)
+{
+    return ina_.getCurrent_mA()/1000.0;
+}
+
+/****************************
+ * global variables
+ ****************************/
 static XBee myXBee = XBee();
 static XBeeAddress64 addrContributor = XBeeAddress64(XBEE_ADDRESS_H_COORDINATOR, XBEE_ADDRESS_L_COORDINATOR);
 
-static INA226 g_ina_solar;
-static INA226 g_ina_battery;
-static Adafruit_INA219 g_ina_converter(INA219_A0A1_ADDRESS);
+static MyINA226 battery = MyINA226("battery", INA226_CC_ADDRESS);
+static MyINA226 solar = MyINA226("solar panel", INA226_DD_ADDRESS);
+static MyINA219 conv = MyINA219("converter", INA219_A0A1_ADDRESS);
+
+static IElectricPower *powerSensors[] =
+{
+    static_cast<IElectricPower*>(&battery),
+    static_cast<IElectricPower*>(&solar),
+    static_cast<IElectricPower*>(&conv)
+};
 
 static unsigned long mainLoopInterval = MAIN_INTERVAL_MSEC;
 
@@ -59,81 +229,6 @@ boolean isTriggerToPost(void)
     return true;
 }
 
-void checkConfig(INA226 ina)
-{
-    DEBUG_PRINT("Mode:                  ");
-    switch (ina.getMode())
-    {
-        case INA226_MODE_POWER_DOWN:      DEBUG_PRINT("Power-Down\n"); break;
-        case INA226_MODE_SHUNT_TRIG:      DEBUG_PRINT("Shunt Voltage, Triggered\n"); break;
-        case INA226_MODE_BUS_TRIG:        DEBUG_PRINT("Bus Voltage, Triggered\n"); break;
-        case INA226_MODE_SHUNT_BUS_TRIG:  DEBUG_PRINT("Shunt and Bus, Triggered\n"); break;
-        case INA226_MODE_ADC_OFF:         DEBUG_PRINT("ADC Off\n"); break;
-        case INA226_MODE_SHUNT_CONT:      DEBUG_PRINT("Shunt Voltage, Continuous\n"); break;
-        case INA226_MODE_BUS_CONT:        DEBUG_PRINT("Bus Voltage, Continuous\n"); break;
-        case INA226_MODE_SHUNT_BUS_CONT:  DEBUG_PRINT("Shunt and Bus, Continuous\n"); break;
-        default: DEBUG_PRINT("unknown\n");
-    }
-
-    DEBUG_PRINT("Samples average:       ");
-    switch (ina.getAverages())
-    {
-        case INA226_AVERAGES_1:           DEBUG_PRINT("1 sample\n"); break;
-        case INA226_AVERAGES_4:           DEBUG_PRINT("4 samples\n"); break;
-        case INA226_AVERAGES_16:          DEBUG_PRINT("16 samples\n"); break;
-        case INA226_AVERAGES_64:          DEBUG_PRINT("64 samples\n"); break;
-        case INA226_AVERAGES_128:         DEBUG_PRINT("128 samples\n"); break;
-        case INA226_AVERAGES_256:         DEBUG_PRINT("256 samples\n"); break;
-        case INA226_AVERAGES_512:         DEBUG_PRINT("512 samples\n"); break;
-        case INA226_AVERAGES_1024:        DEBUG_PRINT("1024 samples\n"); break;
-        default: DEBUG_PRINT("unknown\n");
-    }
-
-    DEBUG_PRINT("Bus conversion time:   ");
-    switch (ina.getBusConversionTime())
-    {
-        case INA226_BUS_CONV_TIME_140US:  DEBUG_PRINT("140uS\n"); break;
-        case INA226_BUS_CONV_TIME_204US:  DEBUG_PRINT("204uS\n"); break;
-        case INA226_BUS_CONV_TIME_332US:  DEBUG_PRINT("332uS\n"); break;
-        case INA226_BUS_CONV_TIME_588US:  DEBUG_PRINT("558uS\n"); break;
-        case INA226_BUS_CONV_TIME_1100US: DEBUG_PRINT("1.100ms\n"); break;
-        case INA226_BUS_CONV_TIME_2116US: DEBUG_PRINT("2.116ms\n"); break;
-        case INA226_BUS_CONV_TIME_4156US: DEBUG_PRINT("4.156ms\n"); break;
-        case INA226_BUS_CONV_TIME_8244US: DEBUG_PRINT("8.244ms\n"); break;
-        default: DEBUG_PRINT("unknown\n");
-    }
-
-    DEBUG_PRINT("Shunt conversion time: ");
-    switch (ina.getShuntConversionTime())
-    {
-        case INA226_SHUNT_CONV_TIME_140US:  DEBUG_PRINT("140uS\n"); break;
-        case INA226_SHUNT_CONV_TIME_204US:  DEBUG_PRINT("204uS\n"); break;
-        case INA226_SHUNT_CONV_TIME_332US:  DEBUG_PRINT("332uS\n"); break;
-        case INA226_SHUNT_CONV_TIME_588US:  DEBUG_PRINT("558uS\n"); break;
-        case INA226_SHUNT_CONV_TIME_1100US: DEBUG_PRINT("1.100ms\n"); break;
-        case INA226_SHUNT_CONV_TIME_2116US: DEBUG_PRINT("2.116ms\n"); break;
-        case INA226_SHUNT_CONV_TIME_4156US: DEBUG_PRINT("4.156ms\n"); break;
-        case INA226_SHUNT_CONV_TIME_8244US: DEBUG_PRINT("8.244ms\n"); break;
-        default: DEBUG_PRINT("unknown\n");
-    }
-
-    DEBUG_PRINT("Max possible current:  ");
-    DEBUG_PRINT(ina.getMaxPossibleCurrent());
-    DEBUG_PRINT(" A\n");
-
-    DEBUG_PRINT("Max current:           ");
-    DEBUG_PRINT(ina.getMaxCurrent());
-    DEBUG_PRINT(" A\n");
-
-    DEBUG_PRINT("Max shunt voltage:     ");
-    DEBUG_PRINT(ina.getMaxShuntVoltage());
-    DEBUG_PRINT(" V\n");
-
-    DEBUG_PRINT("Max power:             ");
-    DEBUG_PRINT(ina.getMaxPower());
-    DEBUG_PRINT(" W\n");
-}
-
 void setup(void)
 {
     setup_debug();
@@ -147,34 +242,19 @@ void setup(void)
     pinMode(LED_SWITCH_PIN, OUTPUT);
     digitalWrite(LED_SWITCH_PIN, LOW);
 
-    g_ina_converter.begin();
-    g_ina_battery.begin(INA226_CC_ADDRESS);
-    g_ina_solar.begin(INA226_DD_ADDRESS);
-
-    // Configure INA226
-    g_ina_battery.configure(
-            INA226_AVERAGES_1,
-            INA226_BUS_CONV_TIME_1100US,
-            INA226_SHUNT_CONV_TIME_1100US,
-            INA226_MODE_SHUNT_BUS_CONT);
-    // Calibrate INA226. Rshunt = 0.002 ohm, Max excepted current = 12A
-    g_ina_battery.calibrate(0.002, 14);
-
-    // Configure INA226
-    g_ina_solar.configure(
-            INA226_AVERAGES_1,
-            INA226_BUS_CONV_TIME_1100US,
-            INA226_SHUNT_CONV_TIME_1100US,
-            INA226_MODE_SHUNT_BUS_CONT);
-    // Calibrate INA226. Rshunt = 0.002 ohm, Max excepted current = 1.11A
-    g_ina_solar.calibrate(0.002, 4);
+    for(auto sensor : powerSensors)
+    {
+        sensor->begin();
+    }
 
 #if (defined(ENABLE_DEBUG) || defined(ENABLE_DEBUG_SOFT_SERIAL))
     delay(5000);
 
     // Show configuration
-    checkConfig(g_ina_battery);
-    checkConfig(g_ina_solar);
+    for(auto sensor : powerSensors)
+    {
+        sensor->showConfig();
+    }
 #endif
 }
 
@@ -186,23 +266,13 @@ void loop(void)
     led_on = !led_on;
 
     if (isTriggerToPost() != true)
+    {
         return;
+    }
 
-    DEBUG_PRINT("battery:");
-    DEBUG_PRINT(" shu A: "); DEBUG_PRINT(g_ina_battery.readShuntCurrent()); DEBUG_PRINT("\n");
-    DEBUG_PRINT(" shu V: "); DEBUG_PRINT(g_ina_battery.readShuntVoltage()); DEBUG_PRINT("\n");
-    DEBUG_PRINT(" bus P: "); DEBUG_PRINT(g_ina_battery.readBusPower());     DEBUG_PRINT("\n");
-    DEBUG_PRINT(" bus V: "); DEBUG_PRINT(g_ina_battery.readBusVoltage());   DEBUG_PRINT("\n");
-
-    DEBUG_PRINT("solar:\n");
-    DEBUG_PRINT(" shu A: "); DEBUG_PRINT(g_ina_solar.readShuntCurrent()); DEBUG_PRINT("\n");
-    DEBUG_PRINT(" shu V: "); DEBUG_PRINT(g_ina_solar.readShuntVoltage()); DEBUG_PRINT("\n");
-    DEBUG_PRINT(" bus P: "); DEBUG_PRINT(g_ina_solar.readBusPower());     DEBUG_PRINT("\n");
-    DEBUG_PRINT(" bus V: "); DEBUG_PRINT(g_ina_solar.readBusVoltage());   DEBUG_PRINT("\n");
-
-    DEBUG_PRINT("charge converter:\n");
-    DEBUG_PRINT(" bus V: "); DEBUG_PRINT(g_ina_converter.getBusVoltage_V());    DEBUG_PRINT("\n");
-    DEBUG_PRINT(" shu V: "); DEBUG_PRINT(g_ina_converter.getShuntVoltage_mV()); DEBUG_PRINT("\n");
-    DEBUG_PRINT(" cur mA: ");DEBUG_PRINT(g_ina_converter.getCurrent_mA());      DEBUG_PRINT("\n");
+    for(auto sensor : powerSensors)
+    {
+        sensor->showStatus();
+    }
 }
 
